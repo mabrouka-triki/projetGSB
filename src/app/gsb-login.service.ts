3070696122150b271238a54f5d7c99815bbca441
mtrik <trikimabrouka09@gmail.com>
2023-12-15 15:45:56 +0100

    liste en cours

--- src/app/gsb-login.service.ts
+++ src/app/gsb-login.service.ts
@@ -1,28 +1,33 @@
-
 import { Injectable } from '@angular/core';
-import { HttpClient } from '@angular/common/http';
-import { Router } from '@angular/router';
-import {Login} from "./metier/login";
-import {Visiteur} from "./metier/visiteur";
-
+import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { BehaviorSubject } from 'rxjs';
+import { GsbLoginService } from './gsb-login.service';
+import { Frais } from "./modeles/frais";
 
 @Injectable({
-  providedIn: 'root',
+  providedIn: 'root'
 })
-export class GsbLoginService {
-  private login: Login = new Login();
+export class GsbFraisService {
+  private _reponses = new BehaviorSubject<Frais[]>([]);
+  readonly appels_termines = this._reponses.asObservable();
+  public listeFrais: Frais[] = [];
 
-  constructor(private http: HttpClient, private router: Router) {}
+  constructor(private http: HttpClient, private gsb_api: GsbLoginService) { }
 
-  serviceEnvoieLogin(email: string, password: string) {
-    const requestObject = new Visiteur({ email: email, password: password });
+  listeFraisDuVisiteur() {
+    const headers = new HttpHeaders({
+      'Authorization': 'Bearer ' + this.gsb_api.recupereBearer()
+    });
 
-    this.http.post<Login>('http://localhost/api/ProjetWSGSB/public/api/login', requestObject).subscribe(
-      (data: Login) => {
-        this.login = new Login(data);
-        this.router.navigate(['/frais/liste']);
+    this.http.get<Frais[]>(
+      'http://localhost/api/ProjetWSGSB/public/api/listeFrais/' + this.gsb_api.visiteurId(),
+      { headers: headers }
+    ).subscribe(
+      data => {
+        this.listeFrais = data;
+        this._reponses.next(this.listeFrais);
       },
-      (error) => console.log('Erreur appel API', error)
+      error => console.log('Erreur appel API', error)
     );
   }
 }
